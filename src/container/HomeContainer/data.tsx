cbb85bb61fc7de9add383f6b452442e54c6ccdf6
Liep Nguyen <liepnguyen@kms-technology.com>
2018-01-02 15:07:55 +0700

    initial commit

---
+++ src/container/HomeContainer/data.tsx
@@ -0,0 +1,35 @@
+export default [
+	"React Native Starter Kit",
+	"React Navigation",
+	"NativeBase Easy Grid",
+	"NativeBase",
+	"CodePush",
+	"Redux",
+];
+
+export const jars = [
+	{
+		name: 'NEC',
+		remaining: 300
+	},
+	{
+		name: 'PLY',
+		remaining: 300
+	},
+	{
+		name: 'FFA',
+		remaining: 300
+	},
+	{
+		name: 'LTS',
+		remaining: 300
+	},
+	{
+		name: 'EDU',
+		remaining: 300
+	},
+	{
+		name: 'GIV',
+		remaining: 300
+	}
+]
